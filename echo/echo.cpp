4ef1e26d53a890879605e2f65d7d18277deec56e
Ruben Van Boxem <vanboxem.ruben@gmail.com>
2013-01-15 21:24:53 +0100

    Correct CMakeLists.txt files contents and add Boost to each of them.

---
+++ echo/echo.cpp
@@ -0,0 +1,20 @@
+/**
+ * Unix Tools For Windows
+ *  - date: write the date and time
+ *
+ * Written in 2013 by Ruben Van Boxem <vanboxem.ruben@gmail.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related
+ * and neighboring rights to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ */
+
+#include <boost/program_options/parsers.hpp>
+
+int main(int argc, char* argv[])
+{
+}
